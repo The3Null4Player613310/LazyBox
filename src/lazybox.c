fb54b7207925384053bcddd703363ef1f7341350
N <nathanmunn21@gmail.com>
2021-08-11 02:53:22 -0300

    Minor changes to LazyBox binary

--- src/lazybox.c
+++ src/lazybox.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 
 #define ARGS_LEN 256
-//#define PROG_LEN 64
+#define PROG_LEN 64
 #define PROG_NUM 3
 
 int echo(int countIn, char *argsIn[]) {
@@ -34,7 +34,7 @@ int resolver(int countIn, char *argsIn[]) {
 
 	int progid = 0;		
 	for(int i=0; i<PROG_NUM; i++) {
-		if (strcmp(progs[i], argsIn[0])) {
+		if (strcmp(progs[i], argsIn[0]) == 0) {
 			progid = i;
 		}
 	}
@@ -52,24 +52,47 @@ int resolver(int countIn, char *argsIn[]) {
 }
 
 int main(int countIn, char *argsIn[]) {
-	//wip
-	char *binary;
-	char *token = strtok(argsIn[0],'\');
-	while(token != NULL) {
-		strcpy(binary, token)
-		token = strtok(NULL,'\');
-	}
-	//wip
 
-	if((strcmp(binary,"lazybox") == 0)) {
-		printf("%s",argsIn[0]);
+	//find size of binary string
+	int size=0;
+	while(argsIn[0][size] != '\0') {
+		size=size+1;
+	}
+	size=size+1;
+
+	//removes leading garbage from binary name
+	char binary[size];
+	char *token = strtok(argsIn[0], "/");
+	while(token != NULL) {
+		strcpy(binary, token);
+		token = strtok(NULL, "/");
+	}
+
+	//clears the trailing characters after null terminator 
+	//hi0llo becomes hi0000 etc
+	int i = 0;
+	while(argsIn[0][i] != '\0') {
+		argsIn[0][i] = '\0';
+		i=i+1;
+	}
+	
+	//moves new string into args
+	strcpy(argsIn[0], binary);
+
+	printf("wub");
+	printf("Running %s\n", argsIn[0]);
+
+	//lazybox mode
+	if((strcmp(argsIn[0], "lazybox") == 0)) {
+		printf("%s", argsIn[0]);
 		int count=countIn-1;
 		char *args[count];
 		for(int i=0; i<count; i++) {
-			strcpy(args[i],argsIn[i+1]);
+			strcpy(args[i], argsIn[i+1]);
 		}
 		return resolver(count, args);
 	}
-
+	
+	//link mode
 	return resolver(countIn, argsIn);
 }
